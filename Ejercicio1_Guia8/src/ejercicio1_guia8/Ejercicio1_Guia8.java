79bf36cf8483f7a7089b1fd6bb1c0dc823f7f844
Emi Albertrelli <emialbertarelli@hotmail.com>
2023-04-21 20:24:44 -0300

    Guia 8

---
+++ Ejercicio1_Guia8/src/ejercicio1_guia8/Ejercicio1_Guia8.java
@@ -0,0 +1,26 @@
+/*Realizar una clase llamada CuentaBancaria en el paquete Entidades con los 
+siguientes atributos: numeroCuenta(entero), dniCliente(entero largo), 
+saldoActual. Agregar constructor vacío, con parámetros, getters y setters.
+Agregar la clase CuentaBancariaServicio en el paquete Servicios que contenga:
+1)Método para crear cuenta pidiéndole los datos al usuario.
+2)Método ingresar(double): recibe una cantidad de dinero a ingresar y se le sumará
+al saldo actual.
+3)4)Método retirar(double): recibe una cantidad de dinero a retirar y se le restara 
+al saldo actual. Si la cuenta no tiene la cantidad de dinero a retirar se retirará 
+el máximo posible hasta dejar la cuenta en 0.
+5)Método extraccionRapida: le permitirá sacar solo un 20% de su saldo. Validar que 
+el usuario no saque más del 20%.
+6)Método consultarSaldo: permitirá consultar el saldo disponible en la cuenta.
+7)Método consultarDatos: permitirá mostrar todos los datos de la cuenta.
+ */
+package ejercicio1_guia8;
+
+
+public class Ejercicio1_Guia8 {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
